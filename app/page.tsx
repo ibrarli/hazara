61f95d1364fab4b06aace945ae0a023101f4d1ff
ibrarli <ibraralihaidar@gmail.com>
2025-07-08 00:32:20 -0700

    update deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Hello Hazara 🌍</h1>
+      <h1>Hello 🌍</h1>
       <p>Your Vercel site is working!</p>
     </main>
   );
