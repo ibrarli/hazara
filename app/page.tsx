b2fde2034b4c3e22ed7d659f4fcabb14445b5ee8
ibrarli <ibraralihaidar@gmail.com>
2025-07-08 00:01:48 -0700

    Add homepage at /

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-
 export default function Home() {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <main style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Hello Hazara 🌍</h1>
+      <p>Your Vercel site is working!</p>
+    </main>
   );
 }
